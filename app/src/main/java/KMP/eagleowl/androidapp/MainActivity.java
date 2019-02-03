a99c399fbccefd7467409249f048daaff37650e1
eagle-owl <bondareff011@gmail.com>
2019-02-03 17:28:36 +0300

    Added Fabric plugin

--- app/src/main/java/KMP/eagleowl/androidapp/MainActivity.java
+++ app/src/main/java/KMP/eagleowl/androidapp/MainActivity.java
@@ -2,6 +2,11 @@ package KMP.eagleowl.androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+    }
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
     }
 }
