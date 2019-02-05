3940215d8fd7c4ecb3a7413f35dfac10a41e2328
eagle-owl <bondareff011@gmail.com>
2019-02-05 12:40:54 +0300

    test 1

--- app/src/main/java/KMP/eagleowl/androidapp/MainActivity.java
+++ app/src/main/java/KMP/eagleowl/androidapp/MainActivity.java
@@ -1,13 +1,17 @@
 package KMP.eagleowl.androidapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
-import android.view.View;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import io.fabric.sdk.android.Fabric;
+
 
 
 
@@ -19,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "47184f3d-64f0-40dd-aca4-f287bc6614eb",
+                Analytics.class, Crashes.class);
+
 
         TextView link = (TextView) findViewById(R.id.textView2);
         if (link != null) {
